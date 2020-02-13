f32ea224ec56792f3cfcfef0b0f6137a858c9232
David Kuseh <davegru203@gmail.com>
2020-02-13 00:59:42 +0000

    make changes to room class

--- src/room.py
+++ src/room.py
@@ -4,10 +4,10 @@
 class Room:
     def __init__(self, name, description):
         self.name = name
-        self.description = description
+        self.description = description 
 
+    def __str__(self):
+        return f"\nYou are in the {self.name}: {self.description}\n"
 
-
-
-
-    
+    def wrong_room(self):
+        print(f" You have entered the wrong room")
